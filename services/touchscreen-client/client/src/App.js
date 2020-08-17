e19d177990559a106e8271d8918f96e1f8f43894
Matthew Lyons <matthewwaynelyons@live.com>
2020-08-17 09:22:28 -0700

    Added Idle redirect

--- services/touchscreen-client/client/src/App.js
+++ services/touchscreen-client/client/src/App.js
@@ -1,6 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 
+import { useIdleTimer } from 'react-idle-timer';
+
+// Views
 import Home from './views/Home';
 import About from './views/About';
 import Category from './views/Category';
@@ -8,34 +11,36 @@ import Room from './views/Room';
 import Search from './views/Search';
 import SearchResults from './views/SearchResults/SearchResults';
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/Room" render={(props) => <Room {...props} />} />
-        <Route exact path="/About">
-          <About />
-        </Route>
-        <Route exact path="/Search">
-          <Search />
-        </Route>
-        <Route
-          exact
-          path="/Search/:query"
-          render={(props) => <SearchResults {...props} />}
-        />
-        <Route
-          exact
-          path="/Category/:id"
-          render={(props) => <Category {...props} />}
-        />
-        <Redirect to="/" />
-      </Router>
-    );
-  }
-}
+export default function App() {
+  // timeout of 10 minutes
+  const timeout = 10 * 60 * 1000;
 
-export default App;
+  const handleOnIdle = () => {
+    if (window.location.pathname !== '/') {
+      window.location = '/';
+    }
+  };
+
+  useIdleTimer({
+    timeout,
+    onIdle: handleOnIdle
+  });
+
+  return (
+    <Router>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/Room" render={(props) => <Room {...props} />} />
+      <Route exact path="/About">
+        <About />
+      </Route>
+      <Route exact path="/Search">
+        <Search />
+      </Route>
+      <Route exact path="/Search/:query" render={(props) => <SearchResults {...props} />} />
+      <Route exact path="/Category/:id" render={(props) => <Category {...props} />} />
+      <Redirect to="/" />
+    </Router>
+  );
+}
