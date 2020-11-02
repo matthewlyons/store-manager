6cfce639e05707d19ff4f10813dadb7abc44c5d1
Matthew Lyons <matthewwaynelyons@live.com>
2020-11-02 10:11:48 -0800

    Client Loading Screen

--- services/manager-client/client/src/components/Loading/Loading.js
+++ services/manager-client/client/src/components/Loading/Loading.js
@@ -1,15 +1,28 @@
-import React, { useContext } from 'react';
-
+import React, { useContext, useEffect } from 'react';
+import { useAlert } from '../../customHooks';
 import { StoreContext } from '../../context/StoreContext';
 
 export default function Loading() {
-  const { state } = useContext(StoreContext);
+  const { state, setLoading } = useContext(StoreContext);
+  const { createAlert } = useAlert();
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (state.loading) {
+        setLoading(false);
+        createAlert('Server Took Too Long To Respond');
+      }
+    }, 10000);
+  }, [state.loading]);
 
   return (
     <React.Fragment>
       {state.loading && (
         <div className="Loading">
-          <h1>Loading</h1>
+          <div className="Text">
+            <h1>Loading</h1>
+          </div>
+          <div className="Background"></div>
         </div>
       )}
     </React.Fragment>
