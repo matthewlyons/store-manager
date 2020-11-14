5b0f2dfd2221ef8cbed76d149b5cc85d6f603a4d
Matthew Lyons <matthewwaynelyons@live.com>
2020-11-13 23:46:12 -0800

    Touchscreen Data Download

---
+++ getTouchscreenData.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+
+const axios = require('axios');
+const path = require('path');
+const fs = require('fs');
+const _ = require('lodash');
+
+const urls = require('./urls.json');
+
+const collectionRoute = path.join(
+  __dirname,
+  '/services/touchscreen-client/client/src/data/'
+);
+
+function checkValid(str) {
+  try {
+    let json = JSON.parse(str);
+    console.log(json.length);
+    if (json.length > 0) {
+      return true;
+    }
+    return false;
+  } catch (e) {
+    return false;
+  }
+}
+
+function getInfo(i = 0) {
+  if (i + 1 > urls.length) return done();
+  let url = urls[i];
+  axios
+    .get(`${process.env.WEBSITE_URL}${url.link}?view=json`)
+    .then((collection) => {
+      console.log('Got The Data For: ' + url.title);
+      let jsonString = JSON.stringify(collection.data);
+      let valid = checkValid(jsonString);
+      if (valid) {
+        console.log('Valid');
+      }
+      fs.writeFileSync(collectionRoute + url.title + '.json', jsonString);
+      setTimeout(() => {
+        return getInfo(i + 1);
+      }, 5000);
+    });
+}
+
+function done() {
+  console.log('Done');
+}
+
+getInfo();
