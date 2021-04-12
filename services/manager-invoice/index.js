ba24326844ac7679b053dcafe2b5196c9f1967f3
Matthew Lyons <MatthewWayneLyons@live.com>
2021-04-12 10:30:59 -0700

    remove db from invoice

--- services/manager-invoice/index.js
+++ services/manager-invoice/index.js
@@ -6,7 +6,6 @@ const bodyParser = require('body-parser');
 const Moment = require('moment');
 const path = require('path');
 const _ = require('lodash');
-const { connectDB } = require('../../common/helpers');
 
 // MongoDB Models
 const { Order, DraftOrder } = require('../../common/models/Order');
@@ -19,9 +18,6 @@ const {
   printPDF
 } = require('./helpers');
 
-connectDB().then((message) => {
-  console.log(`Invoice API: ${message}`);
-});
 
 const app = express();
 
@@ -123,7 +119,7 @@ app.post('/Print/:Type', async (req, res) => {
       })
       .catch((pdf) => {
         return res.status(500).json({
-          errors: [{ message: 'Could Not Print Invoice' }]
+          errors: [{ message: pdf }]
         });
       });
   } else {
